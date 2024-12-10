1bf1a36552e1bf099ca1dcfe2b302e44c500c0b7
donidasha <olehnovicadarja@gmail.com>
2024-12-10 09:30:19 +0200

    Add files via upload

---
+++ 10_12/src/Main.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        batter();
+        sum(12, 58);
+
+
+
+            }
+            public static void sum(int a, int b){
+                int rez = a+b;
+                System.out.println("Skaitļu summa "+a+ " "+b+" ir "+rez);
+            }
+            public static void batter(){
+//                1	Make the cookie batter.
+                System.out.println("Mix the dry ingredients.");
+                System.out.println("Cream the butter and sugar.");
+                System.out.println("Beat in the eggs.");
+                System.out.println("Stir in the dry ingredients.");
+            }
+            public static void bake(){
+                //        2	Bake the cookies.
+                System.out.println("Set the oven temperature.");
+                System.out.println("Set the timer.");
+                System.out.println("Place a batch of cookies into the oven.");
+                System.out.println("Allow the cookies to bake.");
+                System.out.println("Set the oven temperature.");
+                System.out.println("Set the timer.");
+                System.out.println("Place a batch of cookies into the oven.");
+                System.out.println("Allow the cookies to bake.");
+            }
+            public static void frosting(){
+                //        3	Add frosting and sprinkles.
+                System.out.println("Mix ingredients for frosting.");
+                System.out.println("Spread frosting and sprinkles.");
+            }
+        }
+
